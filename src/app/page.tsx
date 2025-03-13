4e6b25a5e44546f2d9b4493751734a9a8c823af4
james <jameswyckoff847@gmail.com>
2025-03-13 06:46:43 -0500

    fix: added lint stage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui";
 
 export default function Home() {
 	return (
-		<div className="text-red-100 text-foood-aa">
+		<div className="text-red-100">
 			<div>test div hello</div>
 
 			<Button>Hello This is Button</Button>
