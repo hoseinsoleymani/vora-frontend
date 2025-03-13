c2b3c2d016cfa2c2b6e35b61e49aff24f2b9f5e6
james <jameswyckoff847@gmail.com>
2025-03-13 04:20:45 -0500

    chore: test BIOME

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui";
 export default function Home() {
 	return (
 		<div className="text-red-100 text-foood-aa">
+			<div>test div hello</div>
+
 			<Button>Hello This is Button</Button>
 		</div>
 	);
