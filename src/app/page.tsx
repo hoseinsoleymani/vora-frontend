56a2981ca4175382d47c4bfb38612037002c8135
Ali Shamsian <alishamsian504@gmail.com>
2025-03-14 21:06:54 +0330

    feat(input-ui): sync with latest develop changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui";
+import { Button, Input } from "@/components/ui";
 
 export default function Home() {
-	return (
-		<div className="text-red-100">
-			<div className="pl-3">test div hello</div>
+  return (
+    <div className="text-red-100">
+      <div className="pl-3">test div hello</div>
 
-			<Button>Hello This is Button</Button>
-		</div>
-	);
+      <Button>Hello This is Button</Button>
+
+      <Input
+        label="Name"
+        placeholder="Enter your name"
+        leftIcon={<svg>👤</svg>}
+      />
+
+      <Input
+        label="Email"
+        placeholder="example@mail.com"
+        variant="outline"
+      />
+
+      <Input
+        label="Password"
+        placeholder="*******"
+        type="password"
+        errorMessage="Password is too short"
+      />
+    </div>
+  );
 }
