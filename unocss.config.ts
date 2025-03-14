f703a9bea6b38cde2355507d96632e8994ee3f85
james <jameswyckoff847@gmail.com>
2025-03-14 10:58:35 -0500

    refactor: Simplify UnoCSS configuration and remove unused dependencies

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, presetAttributify, presetWebFonts, presetWind3, dd , transformerDirectives } from 'unocss'
+import { defineConfig, presetAttributify, presetWebFonts, presetWind3 } from 'unocss'
 import presetAnimations from 'unocss-preset-animations'
 import { presetShadcn } from 'unocss-preset-shadcn'
 
@@ -15,18 +15,12 @@ export default defineConfig({
  presets: [
 		presetWind3(),
 		presetAttributify(),
-		presetWebFonts({
-			provider: "google",
-			fonts: { sans: "Montserrat" },
-		}),
-		presetAnimations(),
 		presetShadcn(),
 	],
-	transformers: [transformerDirectives()],
 	content: {
 		pipeline: {
 			include: [/\.(vue|svelte|[jt]s|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/],
 		},
-		filesystem: ["**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
+		filesystem: ["src/**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}"],
     }
 })
