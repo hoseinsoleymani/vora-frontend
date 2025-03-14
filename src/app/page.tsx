8d6f1e978f9624453cf5693b8317d19c180dd955
james <jameswyckoff847@gmail.com>
2025-03-14 11:19:43 -0500

    fix: uno css crash bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui";
 export default function Home() {
 	return (
 		<div className="text-red-100">
-			<div>test div hello</div>
+			<div className="pl-3">test div hello</div>
 
 			<Button>Hello This is Button</Button>
 		</div>
