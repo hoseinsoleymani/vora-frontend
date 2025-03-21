b18ec8e6c4ebe2554fb1ce5243feb137b0b31436
Hamid Nikoofard <156426727+hamidnikoofard@users.noreply.github.com>
2025-03-22 00:52:51 +0330

    fix : resolve conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,12 @@
-import Faq from "./components/faq";
-import Offer from "./components/offer";
-import PageIntro from "./components/pageIntro";
-import TransportOptions from "./components/transportOptions";
+import { Button } from "@/components/ui";
+
 
 export default function Home() {
-  return (
-    <div className="max-w-screen-xl mx-auto mt-16">
-      <section>
-        <PageIntro />
-      </section>
-      <section className="mt-8">
-        <TransportOptions />
-      </section>
-      <section className="mt-60">
-        <Offer />
-      </section>
-	  <section className="mt-60">
-		<Faq />
-	  </section>
-    </div>
-  );
+	return (
+		<div className="text-red-100">
+			<div className="pl-3">test div hello</div>
+
+			<Button>Hello This is Button</Button>
+		</div>
+	);
 }
