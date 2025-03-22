d0abb8edb84bea16aa677d12aaff20c3e367e865
Hamid Nikoofard <156426727+hamidnikoofard@users.noreply.github.com>
2025-03-22 12:45:18 +0330

    fix : fixing Home page and change serch bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Offer from "./components/offer";
 import PageIntro from "./components/pageIntro";
 import SearchBar from "./components/searchSection/searchBar";
+import Offer from "./components/offer";
+import Faq from "./components/faq"; 
 
 
 
 export default function Home() {
 	return (
-		<div className="text-red-100">
-			<div className="pl-3">test div hello</div>
-
-			<Button>Hello This is Button</Button>
+		<div className="max-w-screen-xl mx-auto mt-16">
+      <section className="mt-60">
+        <PageIntro />
+      </section>
+      <section className="mt-60">
+        <SearchBar />
+      </section>
+      <section className="mt-60">
+        <Offer />
+      </section>
+      <section className="mt-60">
+        <Faq />
+      </section>
 		</div>
 	);
 }
